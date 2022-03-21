882b569a178b7e966dbc3aa3f353d86d0a567e8e
seal9055 <seal9055@gmail.com>
2022-03-20 21:17:02 -0400

    First functional version of the JIT since swapping from original ssa/regalloc implementation. Still has some small bugs but mostly works

--- target/debug/deps/my_libs-ccfc8e85d652627d.d
+++
@@ -1,9 +0,0 @@
-/home/seal/github/sfuzz/target/debug/deps/my_libs-ccfc8e85d652627d.rmeta: /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/lib.rs /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/sorted_vec.rs /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/elf_parser.rs
-
-/home/seal/github/sfuzz/target/debug/deps/libmy_libs-ccfc8e85d652627d.rlib: /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/lib.rs /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/sorted_vec.rs /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/elf_parser.rs
-
-/home/seal/github/sfuzz/target/debug/deps/my_libs-ccfc8e85d652627d.d: /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/lib.rs /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/sorted_vec.rs /home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/elf_parser.rs
-
-/home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/lib.rs:
-/home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/sorted_vec.rs:
-/home/seal/.cargo/git/checkouts/local_crates-3b87952a15cdeb6d/332d166/my_libs/src/elf_parser.rs:
